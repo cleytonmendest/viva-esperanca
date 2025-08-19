f9fb7b8f4ea2733290f33c771c221fa9eb91e03d
Cleyton Mendes <cleyton.mendest@gmail.com>
2025-08-19 20:54:25 -0300

    feat(Header): wip

--- src/components/HeaderMain.tsx
+++ src/components/HeaderMain.tsx
@@ -3,9 +3,12 @@ import Link from 'next/link';
 
 const HeaderMain = () => {
   return (
-    <header className='w-full h-24 flex justify-center mt-6'>
+    <header className='w-full h-36 flex justify-center'>
       <div className='fixed top-0 w-full h-24 bg-[var(--background)] z-10'>
-        <div className='mx-auto w-full lg:max-w-7xl flex justify-between items-center p-4'>
+        <div className='mx-auto w-full lg:max-w-7xl flex flex-col gap-3.5 justify-between items-center p-4'>
+          <Link href="/" className="cursor-pointer">
+            <Image src="/images/logo-ive-branco.png" alt="Logo Viva Esperança" width={200} height={75} />
+          </Link>
           <nav className='w-full'>
             <ul className='flex gap-10 justify-center items-center'>
               <li>
@@ -13,11 +16,6 @@ const HeaderMain = () => {
               </li>
               <li>
                 <Link href="/programacao">GC&#39;s</Link>
-              </li>
-              <li>
-                <Link href="/" className="cursor-pointer">
-                  <Image src="/images/logo-ive-branco.png" alt="Logo Viva Esperança" width={200} height={75} />
-                </Link>
               </li>
               <li>
                 <Link href="/ofertas">DÍZIMOS E OFERTAS</Link>
