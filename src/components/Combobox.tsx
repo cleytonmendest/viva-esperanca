7605ddcf9b084ae4fb3128d961b62ce0791ff9e3
--global <cleyton.mendest@gmail.com>
2025-09-21 23:17:27 -0300

    feat: add component combobox

---
+++ src/components/Combobox.tsx
@@ -0,0 +1,78 @@
+"use client"
+
+import * as React from "react"
+import { Check, ChevronsUpDown } from "lucide-react"
+
+import { cn } from "@/libs/utils"
+import { Button } from "@/components/ui/button"
+import {
+    Command,
+    CommandEmpty,
+    CommandGroup,
+    CommandInput,
+    CommandItem,
+    CommandList,
+} from "@/components/ui/command"
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/ui/popover"
+
+interface ComboboxProps {
+    options: { value: string; label: string }[]
+    placeholder: string
+    empty: string
+    value: string
+    onChange: (value: string) => void
+}
+
+export function Combobox({ empty, options, placeholder, value, onChange }: ComboboxProps) {
+    const [open, setOpen] = React.useState(false)
+
+    return (
+        <Popover open={open} onOpenChange={setOpen}>
+            <PopoverTrigger asChild>
+                <Button
+                    variant="outline"
+                    role="combobox"
+                    aria-expanded={open}
+                    className="w-[200px] justify-between"
+                >
+                    {value
+                        ? options.find((option) => option.value === value)?.label
+                        : placeholder}
+                    <ChevronsUpDown className="opacity-50" />
+                </Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-[200px] p-0">
+                <Command>
+                    <CommandInput placeholder={placeholder} className="h-9" />
+                    <CommandList>
+                        <CommandEmpty>{empty}</CommandEmpty>
+                        <CommandGroup>
+                            {options.map((option) => (
+                                <CommandItem
+                                    key={option.value}
+                                    value={option.value}
+                                    onSelect={(currentValue) => {
+                                        onChange(currentValue === value ? "" : currentValue)
+                                        setOpen(false)
+                                    }}
+                                >
+                                    {option.label}
+                                    <Check
+                                        className={cn(
+                                            "ml-auto",
+                                            value === option.value ? "opacity-100" : "opacity-0"
+                                        )}
+                                    />
+                                </CommandItem>
+                            ))}
+                        </CommandGroup>
+                    </CommandList>
+                </Command>
+            </PopoverContent>
+        </Popover>
+    )
+}
