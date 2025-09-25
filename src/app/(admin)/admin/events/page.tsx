cb96fca5dc0607e62f9c4b9fb55a5ee6d363e17e
--global <cleyton.mendest@gmail.com>
2025-09-24 22:53:20 -0300

    feat: page events wip

--- src/app/(admin)/admin/events/page.tsx
+++ src/app/(admin)/admin/events/page.tsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const EventsPage = () => {
   return (
-    <div>EventsPage</div>
+    <>
+      <section className="lg:max-w-4xl my-4 mx-auto w-full">
+        <h1 className="text-3xl font-bold">Eventos</h1>
+      </section>
+      <section className="lg:max-w-4xl my-4 mx-auto w-full">
+        
+      </section>
+    </>
   )
 }
 
